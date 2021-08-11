73116dc3eff2386a1861b2f8302d2cf01685812d
Bruce-Li7 <lijiaqi7788@gmail.com>
2021-08-12 02:03:24 +1000

    Add a footer section on html.

--- public/env.js
+++ public/env.js
@@ -42,7 +42,7 @@ const getProjects = () => {
 
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
+        let itemToAppend = '<div class="col s6 center-align">'+
     '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
     '</div><div class="card-content">'+
     '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
