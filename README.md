4c9b5cea76fb12a1434792600dbb6617c1ae8773
Bruce-Li7 <lijiaqi7788@gmail.com>
2021-08-12 01:12:33 +1000

    Add card section and add project ability.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# sit725-2021-t2-prac3
+# sit725-2021-t2-prac4
 SIT725 Practical 3
 
-# Commit Functionality
-This time I add a Popup on commit button. When people click the button. There will pop a mini commit webpage to fill the form.
+# Add project Functionality
+This time I add a button to add project for my webpage.
+
+# Add card section
+This time I add a card sction to my html file.
 
 
 # Copyrights
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
+require('dotenv').config()
 var express = require("express");
 var app = express();
+const MongoClient = require('mongodb').MongoClient;
+let projectCollection;
+
+// Database
+const uri = "mongodb+srv://SIT725-week4-prac:devil1126@sit725.1by8m.mongodb.net/SIT725-week4-prac?retryWrites=true&w=majority"
+const client = new MongoClient(uri,{ useNewUrlParser: true })
+
+
+const createCollection = (collectionName) => {
+    client.connect((err,db) => {
+        projectCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('MongoDB Connected')
+        }
+        else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+        }
+    })
+}
 
 users = [];
 
 app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extened: false }));
+// app.use(cors());
+
 
 app.get("/user",(req,res) => {
     res.json({statusCode: 202, data: users, message: "Success"})
@@ -21,10 +44,43 @@ app.post("/user/commit",(req,res) => {
     res.json({statusCode: 202, data: userData, message: "Submitted"})
 });
 
+const insertProjects = (project,callback) => {
+    projectCollection.insert(project,callback);
+}
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+})
+
+app.post('/api/projects',(req,res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Project Successfully added", data: result})
+        }
+    })
+})
+
 
 var port = process.env.port || 3000;
 
 app.listen(port, ()=>{
     console.log('Web server running at: http://localhost:3000');
     console.log('Type Ctrl+C to shut down the web server');
+    createCollection("projects");
 });
